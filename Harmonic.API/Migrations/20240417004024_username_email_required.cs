6fa06527eb2dada604db5da6955dfb1f62e993bf
Yxiox <bruno.dela.goncalves@gmail.com>
2024-04-16 21:46:55 -0300

    username + email required

---
+++ Harmonic.API/Migrations/20240417004024_username_email_required.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Harmonic.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class username_email_required : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "USERS",
+                keyColumn: "UserName",
+                keyValue: null,
+                column: "UserName",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "USERS",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "USERS",
+                keyColumn: "NormalizedUserName",
+                keyValue: null,
+                column: "NormalizedUserName",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedUserName",
+                table: "USERS",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "USERS",
+                keyColumn: "NormalizedEmail",
+                keyValue: null,
+                column: "NormalizedEmail",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedEmail",
+                table: "USERS",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "USERS",
+                keyColumn: "Email",
+                keyValue: null,
+                column: "Email",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "USERS",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "USERS",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedUserName",
+                table: "USERS",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedEmail",
+                table: "USERS",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "USERS",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
