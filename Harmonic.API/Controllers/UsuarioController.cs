6eaf4e9ebeea150c5712548fd5f0fdd906e29a1b
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2024-05-23 19:23:02 -0300

    Create UsuarioController.cs

---
+++ Harmonic.API/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using Harmonic.API.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Harmonic.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsuarioController : ControllerBase
+{
+    private readonly UserManager<HarmonicIdentityUser> _userManager;
+
+    public UsuarioController(UserManager<HarmonicIdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUserInfo()
+    {
+        var user = await _userManager.GetUserAsync(User);
+
+        if(user is null) return Unauthorized();
+
+        var returnData = new
+        {
+            user.PhoneNumber,
+            user.UserName,
+            user.Email,
+            user.PhoneNumberConfirmed,
+            user.EmailConfirmed,
+        };
+
+        return Ok(returnData);
+    }
+}
